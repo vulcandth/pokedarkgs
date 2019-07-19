a24b60061ea5a668f97c0ea99bb2c62992d71ed7
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2019-07-19 07:06:33 -0300

    QoL #10: better structuring of trainer data (allowing splitting it into banks)
    Co-Authored-By: aaaaaa123456789 <aaaaaa123456789@acidch.at>

--- ram/wram.asm
+++ ram/wram.asm
@@ -2565,6 +2565,13 @@ NEXTU
 wBuySellItemPrice::
 wTempMysteryGiftTimer::
 wMagikarpLength:: dw
+
+NEXTU
+; 16-bit wram
+	ds 13
+wOtherTrainerType:: db
+wTrainerGroupBank:: db
+
 ENDU
 
 wTempEnemyMonSpecies::  db
