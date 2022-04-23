d61b994510321a759bb9da5b40edb5978cf65a02
vulcandth <vulcandth@gmail.com>
2022-04-23 13:27:13 -0500

    GS_VERSION

--- constants/misc_constants.asm
+++ constants/misc_constants.asm
@@ -14,7 +14,11 @@ DEF FEMALE EQU 1
 
 ; G/S version ID: 0 = Gold, 1 = Silver (used by checkver)
 ; Mystery Gift uses incremented values 1 and 2
-DEF GS_VERSION EQU 0
+IF DEF(_GOLD)
+	DEF GS_VERSION EQU 0
+ELIF DEF(_SILVER)
+	DEF GS_VERSION EQU 1
+ENDC
 ; Pokémon Pikachu 2, a step counter / virtual pet device (used by Mystery Gift)
 DEF POKEMON_PIKACHU_2_VERSION EQU 3
 DEF RESERVED_GAME_VERSION EQU 4
