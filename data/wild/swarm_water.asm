e449d7351958b7f9f2f28f5e8c3ab9204edbde9f
vulcandth <vulcandth@gmail.com>
2022-08-06 01:21:30 -0500

    Fix swarm_water size

--- data/wild/swarm_water.asm
+++ data/wild/swarm_water.asm
@@ -5,9 +5,9 @@ SwarmWaterWildMons:
 ; Marill swarm
 	def_water_wildmons MOUNT_MORTAR_1F_OUTSIDE
 	db 4 percent ; encounter rate
-	db 20, GOLDEEN
-	db 20, MARILL
-	db 20, SEAKING
+	dbw 20, GOLDEEN
+	dbw 20, MARILL
+	dbw 20, SEAKING
 	end_water_wildmons
 
 	db -1 ; end
