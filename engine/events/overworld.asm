09864794fae4068749a3c655276b722d261abde1
vulcandth <vulcandth@gmail.com>
2022-09-08 21:23:38 -0500

    Bugfix: Surfing directly across a map connection does not load the new map

--- engine/events/overworld.asm
+++ engine/events/overworld.asm
@@ -404,7 +404,6 @@ SurfFromMenuScript:
 	special UpdateTimePals
 
 UsedSurfScript:
-; BUG: Surfing directly across a map connection does not load the new map (see docs/bugs_and_glitches.md)
 	writetext UsedSurfText ; "used SURF!"
 	waitbutton
 	closetext
@@ -416,9 +415,7 @@ UsedSurfScript:
 
 	special UpdatePlayerSprite
 	special PlayMapMusic
-; step into the water (slow_step DIR, step_end)
 	special SurfStartStep
-	applymovement PLAYER, wMovementBuffer
 	end
 
 .stubbed_fn
