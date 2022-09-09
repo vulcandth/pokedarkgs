e231b294aca7552825b69efa215dbfe52502ef14
vulcandth <vulcandth@gmail.com>
2022-09-08 22:20:45 -0500

    Bugfix: CheckOwnMon only checks the first five letters of OT names
    This bug can allow you to talk to Eusine in Celadon City and encounter Ho-Oh with only traded legendary beasts.

--- engine/pokemon/search_owned.asm
+++ engine/pokemon/search_owned.asm
@@ -191,8 +191,7 @@ CheckOwnMon:
 
 	ld hl, wPlayerName
 
-; BUG: CheckOwnMon only checks the first five letters of OT names (see docs/bugs_and_glitches.md)
-rept NAME_LENGTH_JAPANESE - 2
+rept PLAYER_NAME_LENGTH - 2
 	ld a, [de]
 	cp [hl]
 	jr nz, .notfound
