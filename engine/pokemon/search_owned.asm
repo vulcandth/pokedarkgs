2bf46ae66dff0c863e751777ea44b8e6d00bd0e9
vulcandth <vulcandth@gmail.com>
2022-09-08 22:22:05 -0500

    Bugfix: CheckOwnMonAnywhere does not check the Day-Care
    This bug can prevent you from talking to Eusine in Celadon City or encountering Ho-Oh when a caught legendary beast is in the Day-Care.

--- engine/pokemon/search_owned.asm
+++ engine/pokemon/search_owned.asm
@@ -26,7 +26,16 @@ CheckOwnMonAnywhere:
 	and a
 	ret z
 
-; BUG: CheckOwnMon does not check the Day-Care (see docs/bugs_and_glitches.md)
+	ld hl, wBreedMon1Species
+	ld bc, wBreedMon1OT
+	call CheckOwnMon
+	ret c ; found!
+
+	ld hl, wBreedMon2Species
+	ld bc, wBreedMon2OT
+	call CheckOwnMon
+	ret c ; found!
+
 	ld d, a
 	ld e, 0
 	ld hl, wPartyMon1Species
