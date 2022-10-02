aaaaaa123456789/pokecrystal16

Convert Pokémon species in trainer data to 16-bit and update reading routines

engine/overworld/wildmons.asm
@@ -919,7 +919,7 @@ RandomPhoneMon:
 	call GetFarByte
 	inc hl
 	ld c, a
-	ld a, 2
+	ld a, 3
 	bit TRAINERTYPE_ITEM_F, c
 	jr z, .no_item
 	inc a
@@ -958,7 +958,8 @@ RandomPhoneMon:
 
 	inc hl ; species
 	ld a, [wTrainerGroupBank]
-	call GetFarByte
+	call GetFarHalfword
+	call GetPokemonIDFromIndex
 	ld [wNamedObjectIndexBuffer], a
 	call GetPokemonName
 	ld hl, wStringBuffer1

vulcandth/pokedarkgs

Co-Authored-By: aaaaaa123456789 <aaaaaa123456789@acidch.at>

engine/overworld/wildmons.asm
@@ -921,7 +921,7 @@ RandomPhoneMon:
 	call GetFarByte
 	inc hl
 	ld c, a
-	ld a, 2
+	ld a, 3
 	bit TRAINERTYPE_ITEM_F, c
 	jr z, .no_item
 	inc a
@@ -960,7 +960,8 @@ RandomPhoneMon:
 
 	inc hl ; species
 	ld a, [wTrainerGroupBank]
-	call GetFarByte
+	call GetFarWord
+	call GetPokemonIDFromIndex
 	ld [wNamedObjectIndex], a
 	call GetPokemonName
 	ld hl, wStringBuffer1