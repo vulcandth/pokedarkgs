ca17875e8c6e78a8add3fb0bf17de8fdb6457aa5
vulcandth <vulcandth@gmail.com>
2022-12-11 12:49:13 -0600

    Don't waste battery
    Co-Authored-By: luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>

--- engine/battle_anims/anim_commands.asm
+++ engine/battle_anims/anim_commands.asm
@@ -15,14 +15,11 @@ PlayBattleAnim:
 
 _PlayBattleAnim:
 	ld c, 6
-.wait
-	call BattleAnimDelayFrame
-	dec c
-	jr nz, .wait
+	call DelayFrames
 
 	call BattleAnimAssignPals
 	call BattleAnimRequestPals
-	call BattleAnimDelayFrame
+	call DelayFrame
 
 	ld c, 1
 	ldh a, [rKEY1]
@@ -44,9 +41,8 @@ _PlayBattleAnim:
 	ld a, 1
 	ldh [hBGMapMode], a
 
-	call BattleAnimDelayFrame
-	call BattleAnimDelayFrame
-	call BattleAnimDelayFrame
+	ld c, 3
+	call DelayFrames
 	jp WaitSFX
 
 BattleAnimRunScript:
@@ -70,7 +66,7 @@ BattleAnimRunScript:
 	xor a
 	ldh [hSCX], a
 	ldh [hSCY], a
-	call BattleAnimDelayFrame
+	call DelayFrame
 	call BattleAnimRestoreHuds
 
 .disabled
@@ -130,7 +126,7 @@ RunBattleAnimScript:
 	jr nz, .find
 
 .not_rollout
-	call BattleAnimDelayFrame
+	call DelayFrame
 
 .done
 	ld a, [wBattleAnimFlags]
@@ -140,18 +136,16 @@ RunBattleAnimScript:
 	jp BattleAnim_ClearOAM
 
 BattleAnimClearHud:
-	call BattleAnimDelayFrame
+	call DelayFrame
 	call WaitTop
 	call ClearActorHud
 	ld a, $1
 	ldh [hBGMapMode], a
-	call BattleAnimDelayFrame
-	call BattleAnimDelayFrame
-	call BattleAnimDelayFrame
+	call Delay3
 	jp WaitTop
 
 BattleAnimRestoreHuds:
-	call BattleAnimDelayFrame
+	call DelayFrame
 	call WaitTop
 
 	ldh a, [rSVBK]
@@ -159,19 +153,14 @@ BattleAnimRestoreHuds:
 	ld a, BANK(wCurBattleMon) ; aka BANK(wTempMon), BANK(wPartyMon1), and several others
 	ldh [rSVBK], a
 
-; this block should just be "call UpdateBattleHuds"
-	ld hl, UpdateBattleHuds
-	ld a, BANK(UpdatePlayerHUD)
-	rst FarCall
+	call UpdateBattleHuds
 
 	pop af
 	ldh [rSVBK], a
 
 	ld a, $1
 	ldh [hBGMapMode], a
-	call BattleAnimDelayFrame
-	call BattleAnimDelayFrame
-	call BattleAnimDelayFrame
+	call Delay3
 	jp WaitTop
 
 BattleAnimRequestPals:
@@ -189,18 +178,7 @@ BattleAnimRequestPals:
 	ld b, a
 	ld a, [wOBP0]
 	cp b
-	call nz, BattleAnim_SetOBPals
-	ret
-
-BattleAnimDelayFrame:
-; Like DelayFrame but wastes battery life.
-
-	ld a, 1
-	ld [wVBlankOccurred], a
-.wait
-	ld a, [wVBlankOccurred]
-	and a
-	jr nz, .wait
+	jp nz, BattleAnim_SetOBPals
 	ret
 
 ClearActorHud:
@@ -233,7 +211,7 @@ PlaceWindowOverBattleTextbox: ; unreferenced
 	ldh [hBGMapAddress], a
 	ld a, HIGH(vBGMap0)
 	ldh [hBGMapAddress + 1], a
-	jp BattleAnimDelayFrame
+	jp DelayFrame
 
 BattleAnim_ClearOAM:
 	ld a, [wBattleAnimFlags]
@@ -1350,13 +1328,8 @@ ClearBattleAnims::
 ; Clear animation block
 	ld hl, wLYOverrides
 	ld bc, wBattleAnimEnd - wLYOverrides
-.loop
-	ld [hl], 0
-	inc hl
-	dec bc
-	ld a, c
-	or b
-	jr nz, .loop
+	xor a
+	call ByteFill
 
 	ld hl, wFXAnimID
 	ld e, [hl]
@@ -1367,7 +1340,7 @@ ClearBattleAnims::
 	add hl, de
 	call GetBattleAnimPointer
 	call BattleAnimAssignPals
-	jp BattleAnimDelayFrame
+	jp DelayFrame
 
 BattleAnim_RevertPals:
 	call WaitTop
@@ -1381,7 +1354,7 @@ BattleAnim_RevertPals:
 	xor a
 	ldh [hSCX], a
 	ldh [hSCY], a
-	call BattleAnimDelayFrame
+	call DelayFrame
 	ld a, $1
 	ldh [hBGMapMode], a
 	ret
