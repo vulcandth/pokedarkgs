thegsproj/pokecrystal

Add Suicune Route 14 Event

maps/Route14.asm
@@ -3,11 +3,49 @@
 	const ROUTE14_YOUNGSTER
 	const ROUTE14_POKEFAN_M2
 	const ROUTE14_KIM
+	const ROUTE14_EUSINE
+	const ROUTE14_SUICUNE
 
 Route14_MapScripts:
 	def_scene_scripts
+	scene_script .DummyScene0 ; SCENE_ROUTE14_NOTHING
+	scene_script .DummyScene1 ; SCENE_ROUTE14_SUICUNE_AND_EUSINE
 
 	def_callbacks
+
+.DummyScene0:
+	end
+
+.DummyScene1:
+	end
+
+Route14SuicuneScript:
+	showemote EMOTE_SHOCK, PLAYER, 15
+	cry SUICUNE
+	pause 20
+	turnobject PLAYER, RIGHT
+	playsound SFX_WARP_FROM
+	applymovement ROUTE14_SUICUNE, Route14SuicuneSlideUpMovement
+	applymovement PLAYER, Route14PlayerStepUpMovement
+	playsound SFX_WARP_FROM
+	applymovement ROUTE14_SUICUNE, Route14SuicuneRunAwayMovement
+	disappear ROUTE14_SUICUNE
+	appear ROUTE14_EUSINE
+	playmusic MUSIC_MYSTICALMAN_ENCOUNTER
+	applymovement ROUTE14_EUSINE, Route14EusineRunUpMovement
+	turnobject PLAYER, LEFT
+	applymovement ROUTE14_EUSINE, Route14EusineWalkDownFacePlayerMovement
+	opentext
+	writetext Route14EusineText
+	closetext
+	applymovement ROUTE14_EUSINE, Route14EusineLeaveMovement
+	disappear ROUTE14_EUSINE
+	pause 20
+	special FadeOutMusic
+	special RestartMapMusic
+	pause 10
+	setscene SCENE_ROUTE14_NOTHING
+	end
 
 Kim:
 	faceplayer
@@ -49,6 +87,48 @@ TrainerPokefanmTrevor:
 	waitbutton
 	closetext
 	end
+
+Route14SuicuneSlideUpMovement:
+	set_sliding
+	fast_jump_step UP
+	remove_sliding
+	step_end
+
+Route14PlayerStepUpMovement:
+	step UP
+	step UP
+	step_end
+
+Route14SuicuneRunAwayMovement:
+	set_sliding
+	fast_jump_step UP
+	fast_jump_step UP
+	remove_sliding
+	step_end
+
+Route14EusineRunUpMovement:
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	step_end
+
+Route14EusineWalkDownFacePlayerMovement:
+	step DOWN
+	step RIGHT
+	step_end
+
+Route14EusineLeaveMovement:
+	step LEFT
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	step_end
 
 PokefanmCarterSeenText:
 	text "Let me tell you,"
@@ -113,12 +193,60 @@ PokefanmTrevorAfterBattleText:
 	line "got my #MON…"
 	done
 
+Route14EusineText:
+	text "Eusine: <PLAYER>!"
+	line "Not again!"
+
+	para "I'll be there"
+	line "first next time!"
+
+	para "Having followed it"
+	line "here, I'm starting"
+
+	para "to understand what"
+	line "SUICUNE is after."
+
+	para "To be honest, I"
+	line "would like to keep"
+
+	para "this information"
+	line "to myself."
+
+	para "But I want to be"
+	line "an honest Trainer"
+
+	para "in front of"
+	line "SUICUNE!"
+
+	para "That's why I am"
+	line "sharing a clue"
+	cont "with you."
+
+	para "It seems that…"
+	line "SUICUNE prefers a"
+
+	para "hilly place near"
+	line "water…"
+	cont "…Somewhere north."
+
+	para "I don't know where"
+	line "yet, but it will"
+
+	para "be just you and"
+	line "me!"
+
+	para "Who'll find it"
+	line "first? I challenge"
+	cont "you!"
+	done
+
 Route14_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
 
 	def_coord_events
+	coord_event 16, 14, SCENE_ROUTE14_SUICUNE_AND_EUSINE, Route14SuicuneScript
 
 	def_bg_events
 
@@ -127,3 +255,5 @@ Route14_MapEvents:
 	object_event 11, 27, SPRITE_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperRoy, -1
 	object_event  6, 11, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 3, TrainerPokefanmTrevor, -1
 	object_event  7,  5, SPRITE_TEACHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 4, Kim, -1
+	object_event 13, 17, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_ROUTE_14_EUSINE
+	object_event 19, 14, SPRITE_SUICUNE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_SAW_SUICUNE_ON_ROUTE_14

vulcandth/pokedarkgs

maps/Route14.asm
@@ -3,11 +3,49 @@
 	const ROUTE14_YOUNGSTER
 	const ROUTE14_POKEFAN_M2
 	const ROUTE14_KIM
+	const ROUTE14_EUSINE
+	const ROUTE14_SUICUNE
 
 Route14_MapScripts:
 	def_scene_scripts
+	scene_script Route14Noop1Script, SCENE_ROUTE14_NOOP
+	scene_script Route14Noop2Script, SCENE_ROUTE14_SUICUNE_AND_EUSINE
 
 	def_callbacks
+
+Route14Noop1Script:
+	end
+
+Route14Noop2Script:
+	end
+
+Route14SuicuneScript:
+	showemote EMOTE_SHOCK, PLAYER, 15
+	cry SUICUNE
+	pause 20
+	turnobject PLAYER, RIGHT
+	playsound SFX_WARP_FROM
+	applymovement ROUTE14_SUICUNE, Route14SuicuneSlideUpMovement
+	applymovement PLAYER, Route14PlayerStepUpMovement
+	playsound SFX_WARP_FROM
+	applymovement ROUTE14_SUICUNE, Route14SuicuneRunAwayMovement
+	disappear ROUTE14_SUICUNE
+	appear ROUTE14_EUSINE
+	playmusic MUSIC_MYSTICALMAN_ENCOUNTER
+	applymovement ROUTE14_EUSINE, Route14EusineRunUpMovement
+	turnobject PLAYER, LEFT
+	applymovement ROUTE14_EUSINE, Route14EusineWalkDownFacePlayerMovement
+	opentext
+	writetext Route14EusineText
+	closetext
+	applymovement ROUTE14_EUSINE, Route14EusineLeaveMovement
+	disappear ROUTE14_EUSINE
+	pause 20
+	special FadeOutMusic
+	special RestartMapMusic
+	pause 10
+	setscene SCENE_ROUTE14_NOOP
+	end
 
 Kim:
 	faceplayer
@@ -49,6 +87,48 @@ TrainerPokefanmTrevor:
 	waitbutton
 	closetext
 	end
+
+Route14SuicuneSlideUpMovement:
+	set_sliding
+	fast_jump_step UP
+	remove_sliding
+	step_end
+
+Route14PlayerStepUpMovement:
+	step UP
+	step UP
+	step_end
+
+Route14SuicuneRunAwayMovement:
+	set_sliding
+	fast_jump_step UP
+	fast_jump_step UP
+	remove_sliding
+	step_end
+
+Route14EusineRunUpMovement:
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	step_end
+
+Route14EusineWalkDownFacePlayerMovement:
+	step DOWN
+	step RIGHT
+	step_end
+
+Route14EusineLeaveMovement:
+	step LEFT
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	big_step UP
+	step_end
 
 PokefanmCarterSeenText:
 	text "Let me tell you,"
@@ -113,12 +193,60 @@ PokefanmTrevorAfterBattleText:
 	line "got my #MON…"
 	done
 
+Route14EusineText:
+	text "Eusine: <PLAYER>!"
+	line "Not again!"
+
+	para "I'll be there"
+	line "first next time!"
+
+	para "Having followed it"
+	line "here, I'm starting"
+
+	para "to understand what"
+	line "SUICUNE is after."
+
+	para "To be honest, I"
+	line "would like to keep"
+
+	para "this information"
+	line "to myself."
+
+	para "But I want to be"
+	line "an honest Trainer"
+
+	para "in front of"
+	line "SUICUNE!"
+
+	para "That's why I am"
+	line "sharing a clue"
+	cont "with you."
+
+	para "It seems that…"
+	line "SUICUNE prefers a"
+
+	para "hilly place near"
+	line "water…"
+	cont "…Somewhere north."
+
+	para "I don't know where"
+	line "yet, but it will"
+
+	para "be just you and"
+	line "me!"
+
+	para "Who'll find it"
+	line "first? I challenge"
+	cont "you!"
+	done
+
 Route14_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
 
 	def_coord_events
+	coord_event 16, 14, SCENE_ROUTE14_SUICUNE_AND_EUSINE, Route14SuicuneScript
 
 	def_bg_events
 
@@ -127,3 +255,5 @@ Route14_MapEvents:
 	object_event 11, 27, SPRITE_YOUNGSTER, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_TRAINER, 3, TrainerBirdKeeperRoy, -1
 	object_event  6, 11, SPRITE_POKEFAN_M, SPRITEMOVEDATA_SPINRANDOM_FAST, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_TRAINER, 3, TrainerPokefanmTrevor, -1
 	object_event  7,  5, SPRITE_TEACHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 1, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 4, Kim, -1
+	object_event 13, 17, SPRITE_SUPER_NERD, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_ROUTE_14_EUSINE
+	object_event 19, 14, SPRITE_SUICUNE, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, PAL_NPC_BLUE, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_SAW_SUICUNE_ON_ROUTE_14