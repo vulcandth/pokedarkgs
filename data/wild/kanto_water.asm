thegsproj/pokecrystal

GS Wild Encounters & Swarms
GS Wild Encounters & Swarms

data/wild/kanto_water.asm
@@ -2,20 +2,6 @@
 
 KantoWaterWildMons:
 
-	def_water_wildmons TOHJO_FALLS
-	db 4 percent ; encounter rate
-	db 20, GOLDEEN
-	db 20, SLOWPOKE
-	db 20, SEAKING
-	end_water_wildmons
-
-	def_water_wildmons VERMILION_PORT
-	db 2 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
-	end_water_wildmons
-
 	def_water_wildmons ROUTE_4
 	db 4 percent ; encounter rate
 	db 10, GOLDEEN
@@ -114,6 +100,13 @@ KantoWaterWildMons:
 	db 20, TENTACRUEL
 	end_water_wildmons
 
+	def_water_wildmons TOHJO_FALLS
+	db 4 percent ; encounter rate
+	db 20, GOLDEEN
+	db 20, SLOWPOKE
+	db 20, SEAKING
+	end_water_wildmons
+
 	def_water_wildmons ROUTE_28
 	db 2 percent ; encounter rate
 	db 40, POLIWAG
@@ -165,6 +158,13 @@ KantoWaterWildMons:
 
 	def_water_wildmons CINNABAR_ISLAND
 	db 6 percent ; encounter rate
+	db 35, TENTACOOL
+	db 30, TENTACOOL
+	db 35, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons VERMILION_PORT
+	db 2 percent ; encounter rate
 	db 35, TENTACOOL
 	db 30, TENTACOOL
 	db 35, TENTACRUEL

data/wild/swarm_grass.asm
@@ -2,36 +2,8 @@
 
 SwarmGrassWildMons:
 
-; Dunsparce swarm
-	map_id DARK_CAVE_VIOLET_ENTRANCE
-	db 4 percent, 4 percent, 4 percent ; encounter rates: morn/day/nite
-	; morn
-	db 3, GEODUDE
-	db 3, DUNSPARCE
-	db 2, ZUBAT
-	db 2, GEODUDE
-	db 2, DUNSPARCE
-	db 4, DUNSPARCE
-	db 4, DUNSPARCE
-	; day
-	db 3, GEODUDE
-	db 3, DUNSPARCE
-	db 2, ZUBAT
-	db 2, GEODUDE
-	db 2, DUNSPARCE
-	db 4, DUNSPARCE
-	db 4, DUNSPARCE
-	; nite
-	db 3, GEODUDE
-	db 3, DUNSPARCE
-	db 2, ZUBAT
-	db 2, GEODUDE
-	db 2, DUNSPARCE
-	db 4, DUNSPARCE
-	db 4, DUNSPARCE
-
 ; Yanma swarm
-	map_id ROUTE_35
+	def_grass_wildmons ROUTE_35
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
 	db 12, NIDORAN_M
@@ -57,5 +29,120 @@ SwarmGrassWildMons:
 	db 14, HOOTHOOT
 	db 10, DITTO
 	db 10, DITTO
+	end_grass_wildmons
+
+; Snubbull swarm
+	def_grass_wildmons ROUTE_38
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	IF DEF(_GOLD)
+	; morn
+	db 16, SNUBBULL
+	db 16, RATICATE
+	db 16, MAGNEMITE
+	db 16, FARFETCH_D
+	db 13, MILTANK
+	db 13, TAUROS
+	db 13, TAUROS
+	; day
+	db 16, SNUBBULL
+	db 16, RATICATE
+	db 16, MAGNEMITE
+	db 16, FARFETCH_D
+	db 13, MILTANK
+	db 13, TAUROS
+	db 13, TAUROS
+	; nite
+	db 16, SNUBBULL
+	db 16, RATICATE
+	db 16, MAGNEMITE
+	db 16, RATTATA
+	db 13, MILTANK
+	db 13, TAUROS
+	db 13, TAUROS
+	ELIF DEF(_SILVER)
+	; morn
+	db 16, SNUBBULL
+	db 16, MEOWTH
+	db 16, MAGNEMITE
+	db 16, FARFETCH_D
+	db 13, MILTANK
+	db 13, TAUROS
+	db 13, TAUROS
+	; day
+	db 16, SNUBBULL
+	db 16, MEOWTH
+	db 16, MAGNEMITE
+	db 16, FARFETCH_D
+	db 13, MILTANK
+	db 13, TAUROS
+	db 13, TAUROS
+	; nite
+	db 16, SNUBBULL
+	db 16, MEOWTH
+	db 16, MAGNEMITE
+	db 16, MEOWTH
+	db 13, MILTANK
+	db 13, TAUROS
+	db 13, TAUROS
+	ENDC
+	end_grass_wildmons
+
+; Dunsparce swarm
+	def_grass_wildmons DARK_CAVE_VIOLET_ENTRANCE
+	db 4 percent, 4 percent, 4 percent ; encounter rates: morn/day/nite
+	; morn
+	db 3, GEODUDE
+	db 3, DUNSPARCE
+	db 2, ZUBAT
+	db 2, GEODUDE
+	db 2, DUNSPARCE
+	db 4, DUNSPARCE
+	db 4, DUNSPARCE
+	; day
+	db 3, GEODUDE
+	db 3, DUNSPARCE
+	db 2, ZUBAT
+	db 2, GEODUDE
+	db 2, DUNSPARCE
+	db 4, DUNSPARCE
+	db 4, DUNSPARCE
+	; nite
+	db 3, GEODUDE
+	db 3, DUNSPARCE
+	db 2, ZUBAT
+	db 2, GEODUDE
+	db 2, DUNSPARCE
+	db 4, DUNSPARCE
+	db 4, DUNSPARCE
+	end_grass_wildmons
+
+; Marill swarm
+	def_grass_wildmons MOUNT_MORTAR_1F_OUTSIDE
+	db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+	; morn
+	db 13, MARILL
+	db 15, ZUBAT
+	db 14, MACHOP
+	db 15, MARILL
+	db 14, GEODUDE
+	db 16, RATTATA
+	db 16, RATTATA
+	; day
+	db 13, MARILL
+	db 15, ZUBAT
+	db 14, MACHOP
+	db 15, MARILL
+	db 14, GEODUDE
+	db 16, RATTATA
+	db 16, RATTATA
+	; nite
+	db 13, MARILL
+	db 15, ZUBAT
+	db 14, MACHOP
+	db 15, MARILL
+	db 14, GEODUDE
+	db 16, RATTATA
+	db 16, RATTATA
+	end_grass_wildmons
 
 	db -1 ; end

data/wild/treemons.asm
@@ -108,22 +108,42 @@ TreeMonSet_Lake:
 	db -1
 
 TreeMonSet_Forest:
+IF DEF(_GOLD)
 ; common
-	db 50, HOOTHOOT,   10
-	db 15, PINECO,     10
-	db 15, PINECO,     10
-	db 10, NOCTOWL,    10
+	db 50, CATERPIE,   10
+	db 15, CATERPIE,   10
+	db 15, METAPOD,    10
+	db 10, EXEGGCUTE,  10
+	db  5, EXEGGCUTE,  10
 	db  5, BUTTERFREE, 10
+	db -1
+; rare
+	db 50, CATERPIE,   10
+	db 15, PINECO,     10
+	db 15, PINECO,     10
+	db 10, EXEGGCUTE,  10
+	db  5, EXEGGCUTE,  10
+	db  5, BUTTERFREE, 10
+	db -1
+
+ELIF DEF(_SILVER)
+; common
+	db 50, WEEDLE,     10
+	db 15, WEEDLE,     10
+	db 15, KAKUNA,     10
+	db 10, EXEGGCUTE,  10
+	db  5, EXEGGCUTE,  10
 	db  5, BEEDRILL,   10
 	db -1
 ; rare
-	db 50, HOOTHOOT,   10
-	db 15, CATERPIE,   10
-	db 15, WEEDLE,     10
-	db 10, HOOTHOOT,   10
-	db  5, METAPOD,    10
-	db  5, KAKUNA,     10
+	db 50, WEEDLE,     10
+	db 15, PINECO,     10
+	db 15, PINECO,     10
+	db 10, EXEGGCUTE,  10
+	db  5, EXEGGCUTE,  10
+	db  5, BEEDRILL,   10
 	db -1
+ENDC
 
 TreeMonSet_Rock:
 	db 90, KRABBY,     15

vulcandth/pokedarkgs

data/wild/kanto_water.asm
@@ -2,20 +2,6 @@
 
 KantoWaterWildMons:
 
-	def_water_wildmons TOHJO_FALLS
-	db 4 percent ; encounter rate
-	dbw 20, GOLDEEN
-	dbw 20, SLOWPOKE
-	dbw 20, SEAKING
-	end_water_wildmons
-
-	def_water_wildmons VERMILION_PORT
-	db 2 percent ; encounter rate
-	dbw 35, TENTACOOL
-	dbw 30, TENTACOOL
-	dbw 35, TENTACRUEL
-	end_water_wildmons
-
 	def_water_wildmons ROUTE_4
 	db 4 percent ; encounter rate
 	dbw 10, GOLDEEN
@@ -114,6 +100,13 @@ KantoWaterWildMons:
 	dbw 20, TENTACRUEL
 	end_water_wildmons
 
+	def_water_wildmons TOHJO_FALLS
+	db 4 percent ; encounter rate
+	dbw 20, GOLDEEN
+	dbw 20, SLOWPOKE
+	dbw 20, SEAKING
+	end_water_wildmons
+
 	def_water_wildmons ROUTE_28
 	db 2 percent ; encounter rate
 	dbw 40, POLIWAG
@@ -165,6 +158,13 @@ KantoWaterWildMons:
 
 	def_water_wildmons CINNABAR_ISLAND
 	db 6 percent ; encounter rate
+	dbw 35, TENTACOOL
+	dbw 30, TENTACOOL
+	dbw 35, TENTACRUEL
+	end_water_wildmons
+
+	def_water_wildmons VERMILION_PORT
+	db 2 percent ; encounter rate
 	dbw 35, TENTACOOL
 	dbw 30, TENTACOOL
 	dbw 35, TENTACRUEL

data/wild/swarm_grass.asm
@@ -2,36 +2,8 @@
 
 SwarmGrassWildMons:
 
-; Dunsparce swarm
-	map_id DARK_CAVE_VIOLET_ENTRANCE
-	db 4 percent, 4 percent, 4 percent ; encounter rates: morn/day/nite
-	; morn
-	dbw 3, GEODUDE
-	dbw 3, DUNSPARCE
-	dbw 2, ZUBAT
-	dbw 2, GEODUDE
-	dbw 2, DUNSPARCE
-	dbw 4, DUNSPARCE
-	dbw 4, DUNSPARCE
-	; day
-	dbw 3, GEODUDE
-	dbw 3, DUNSPARCE
-	dbw 2, ZUBAT
-	dbw 2, GEODUDE
-	dbw 2, DUNSPARCE
-	dbw 4, DUNSPARCE
-	dbw 4, DUNSPARCE
-	; nite
-	dbw 3, GEODUDE
-	dbw 3, DUNSPARCE
-	dbw 2, ZUBAT
-	dbw 2, GEODUDE
-	dbw 2, DUNSPARCE
-	dbw 4, DUNSPARCE
-	dbw 4, DUNSPARCE
-
 ; Yanma swarm
-	map_id ROUTE_35
+	def_grass_wildmons ROUTE_35
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
 	dbw 12, NIDORAN_M
@@ -57,5 +29,120 @@ SwarmGrassWildMons:
 	dbw 14, HOOTHOOT
 	dbw 10, DITTO
 	dbw 10, DITTO
+	end_grass_wildmons
+
+; Snubbull swarm
+	def_grass_wildmons ROUTE_38
+	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
+	IF DEF(_GOLD)
+	; morn
+	dbw 16, SNUBBULL
+	dbw 16, RATICATE
+	dbw 16, MAGNEMITE
+	dbw 16, FARFETCH_D
+	dbw 13, MILTANK
+	dbw 13, TAUROS
+	dbw 13, TAUROS
+	; day
+	dbw 16, SNUBBULL
+	dbw 16, RATICATE
+	dbw 16, MAGNEMITE
+	dbw 16, FARFETCH_D
+	dbw 13, MILTANK
+	dbw 13, TAUROS
+	dbw 13, TAUROS
+	; nite
+	dbw 16, SNUBBULL
+	dbw 16, RATICATE
+	dbw 16, MAGNEMITE
+	dbw 16, RATTATA
+	dbw 13, MILTANK
+	dbw 13, TAUROS
+	dbw 13, TAUROS
+	ELIF DEF(_SILVER)
+	; morn
+	dbw 16, SNUBBULL
+	dbw 16, MEOWTH
+	dbw 16, MAGNEMITE
+	dbw 16, FARFETCH_D
+	dbw 13, MILTANK
+	dbw 13, TAUROS
+	dbw 13, TAUROS
+	; day
+	dbw 16, SNUBBULL
+	dbw 16, MEOWTH
+	dbw 16, MAGNEMITE
+	dbw 16, FARFETCH_D
+	dbw 13, MILTANK
+	dbw 13, TAUROS
+	dbw 13, TAUROS
+	; nite
+	dbw 16, SNUBBULL
+	dbw 16, MEOWTH
+	dbw 16, MAGNEMITE
+	dbw 16, MEOWTH
+	dbw 13, MILTANK
+	dbw 13, TAUROS
+	dbw 13, TAUROS
+	ENDC
+	end_grass_wildmons
+
+; Dunsparce swarm
+	def_grass_wildmons DARK_CAVE_VIOLET_ENTRANCE
+	db 4 percent, 4 percent, 4 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 3, GEODUDE
+	dbw 3, DUNSPARCE
+	dbw 2, ZUBAT
+	dbw 2, GEODUDE
+	dbw 2, DUNSPARCE
+	dbw 4, DUNSPARCE
+	dbw 4, DUNSPARCE
+	; day
+	dbw 3, GEODUDE
+	dbw 3, DUNSPARCE
+	dbw 2, ZUBAT
+	dbw 2, GEODUDE
+	dbw 2, DUNSPARCE
+	dbw 4, DUNSPARCE
+	dbw 4, DUNSPARCE
+	; nite
+	dbw 3, GEODUDE
+	dbw 3, DUNSPARCE
+	dbw 2, ZUBAT
+	dbw 2, GEODUDE
+	dbw 2, DUNSPARCE
+	dbw 4, DUNSPARCE
+	dbw 4, DUNSPARCE
+	end_grass_wildmons
+
+; Marill swarm
+	def_grass_wildmons MOUNT_MORTAR_1F_OUTSIDE
+	db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
+	; morn
+	dbw 13, MARILL
+	dbw 15, ZUBAT
+	dbw 14, MACHOP
+	dbw 15, MARILL
+	dbw 14, GEODUDE
+	dbw 16, RATTATA
+	dbw 16, RATTATA
+	; day
+	dbw 13, MARILL
+	dbw 15, ZUBAT
+	dbw 14, MACHOP
+	dbw 15, MARILL
+	dbw 14, GEODUDE
+	dbw 16, RATTATA
+	dbw 16, RATTATA
+	; nite
+	dbw 13, MARILL
+	dbw 15, ZUBAT
+	dbw 14, MACHOP
+	dbw 15, MARILL
+	dbw 14, GEODUDE
+	dbw 16, RATTATA
+	dbw 16, RATTATA
+	end_grass_wildmons
 
 	db -1 ; end

data/wild/treemons.asm
@@ -108,22 +108,42 @@ TreeMonSet_Lake:
 	db -1
 
 TreeMonSet_Forest:
+IF DEF(_GOLD)
 ; common
-	dbbw 50, 10, HOOTHOOT
-	dbbw 15, 10, PINECO
-	dbbw 15, 10, PINECO
-	dbbw 10, 10, NOCTOWL
-	dbbw  5, 10, BUTTERFREE
-	dbbw  5, 10, BEEDRILL
+	dbbw 50, CATERPIE,   10
+	dbbw 15, CATERPIE,   10
+	dbbw 15, METAPOD,    10
+	dbbw 10, EXEGGCUTE,  10
+	dbbw  5, EXEGGCUTE,  10
+	dbbw  5, BUTTERFREE, 10
 	db -1
 ; rare
-	dbbw 50, 10, HOOTHOOT
-	dbbw 15, 10, CATERPIE
-	dbbw 15, 10, WEEDLE
-	dbbw 10, 10, HOOTHOOT
-	dbbw  5, 10, METAPOD
-	dbbw  5, 10, KAKUNA
+	dbbw 50, CATERPIE,   10
+	dbbw 15, PINECO,     10
+	dbbw 15, PINECO,     10
+	dbbw 10, EXEGGCUTE,  10
+	dbbw  5, EXEGGCUTE,  10
+	dbbw  5, BUTTERFREE, 10
 	db -1
+
+ELIF DEF(_SILVER)
+; common
+	dbbw 50, WEEDLE,     10
+	dbbw 15, WEEDLE,     10
+	dbbw 15, KAKUNA,     10
+	dbbw 10, EXEGGCUTE,  10
+	dbbw  5, EXEGGCUTE,  10
+	dbbw  5, BEEDRILL,   10
+	db -1
+; rare
+	dbbw 50, WEEDLE,     10
+	dbbw 15, PINECO,     10
+	dbbw 15, PINECO,     10
+	dbbw 10, EXEGGCUTE,  10
+	dbbw  5, EXEGGCUTE,  10
+	dbbw  5, BEEDRILL,   10
+	db -1
+ENDC
 
 TreeMonSet_Rock:
 	dbbw 90, 15, KRABBY