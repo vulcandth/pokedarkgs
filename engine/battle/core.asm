3d35b06305565556ba9383d6beb6569fb2de0183
vulcandth <vulcandth@gmail.com>
2022-12-28 21:55:50 -0600

    Gain experience from catching Pokémon
    Resolves #25

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -2256,6 +2256,8 @@ UpdateBattleStateAndExperienceAfterEnemyFaint:
 	ld a, [wBattleResult]
 	and BATTLERESULT_BITMASK
 	ld [wBattleResult], a ; WIN
+	; fallthrough
+ApplyExperienceAfterEnemyCaught:
 	call IsAnyMonHoldingExpShare
 	jr z, .skip_exp
 	ld hl, wEnemyMonBaseStats
