5048477903740ddc56e8c22d26fc5be75362b6ea
vulcandth <vulcandth@gmail.com>
2022-12-28 20:58:23 -0600

    Allow tiles to have different attributes in different blocks (including X and Y flip)
    Resolves #21
    
    Co-Authored-By: Rangi <35663410+Rangi42@users.noreply.github.com>

--- mobile/mobile_5f.asm
+++ mobile/mobile_5f.asm
@@ -641,7 +641,7 @@ Function17d370:
 	ld a, BANK(s6_a006)
 	call OpenSRAM
 	ld hl, s6_a006
-	ld de, w4_d000
+;	ld de, w4_d000
 	ld bc, $1000
 	call CopyBytes
 	jp CloseSRAM
@@ -1181,7 +1181,7 @@ Function17d78d:
 	call OpenSRAM
 	ld hl, s6_a006
 	add hl, bc
-	ld de, w4_d000
+;	ld de, w4_d000
 	ld bc, $1000
 	call CopyBytes
 	call CloseSRAM
